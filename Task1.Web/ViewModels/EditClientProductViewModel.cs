024d5e69a34605344a0587ba4fa40e5a2964eb82
Andrew Adel <Andrew_201900191@fci.helwan.edu.eg>
2024-10-17 17:17:20 +0300

    Edit ClientProduct

--- Task1.Web/ViewModels/EditClientProductViewModel.cs
+++ Task1.Web/ViewModels/EditClientProductViewModel.cs
@@ -1,23 +1,20 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-
+﻿using System.ComponentModel.DataAnnotations;
+using UoN.ExpressiveAnnotations.NetCore.Attributes;
 
 namespace Task1.Web.ViewModels
 {
-    public class ClientProductsDetailsViewModel
+    public class EditClientProductViewModel
     {
+        public int ClientId { get; set; }
         public int ProductId { get; set; }
         [MaxLength(50)]
         public string Name { get; set; } = null!;
-        [MaxLength(150)]
-        public string Description { get; set; } = null!;
-        public bool IsActive { get; set; }
-
-        [Display(Name= "Start Date")]
-
+        [Display(Name = "Start Date")]
+        [AssertThat("StartDate >= Today()")]
         public DateTime StartDate { get; set; }
 
         [Display(Name = "End Date")]
+        [AssertThat("EndDate >= Today()")]
         public DateTime? EndDate { get; set; }
         public string Lisence { get; set; } = null!;
     }
